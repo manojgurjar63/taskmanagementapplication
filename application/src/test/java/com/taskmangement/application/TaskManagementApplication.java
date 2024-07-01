7f25c1f85fe69411ee98aa01c1ed56e812ab07fb
manojgurjar63 <manoj.bidhudi22@gmail.com>
2024-07-01 13:49:03 +0530

    Add new Branch

---
+++ application/src/test/java/com/taskmangement/application/TaskManagementApplication.java
@@ -0,0 +1,12 @@
+package com.taskmangement.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TaskManagementApplication {
+	public static void main(String[] args){
+		SpringApplication.run(TaskManagementApplication.class,args);
+	}
+}
